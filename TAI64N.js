e828151cef17ae578c37f438def9f9f59a314049
Taisuke Fukuno <fukuno@jig.jp>
2021-12-22 19:51:04 +0900

    add support long term date

--- TAI64N.js
+++ TAI64N.js
@@ -13,18 +13,16 @@ class TAI64N {
     if (tai64n.length != 12) {
       throw new Error("is not length 12 bytes");
     }
-    if (tai64n[0] != 0x40) {
-      throw new Error("not supported yet before 1970");
-    }
   }
   static encode(sec, nsec) {
     if (sec instanceof Date) {
       const t = sec.getTime();
       sec = Math.floor(t / 1000);
       nsec = (t - sec * 1000) * 1000 * 1000;
+      sec = BigInt(sec);
     }
     const offset = TAI64N.getOffsetByLeap(sec);
-    sec += offset;
+    sec += 0x4000000000000000n + offset;
 
     const hexsec = fix0(sec.toString(16), 16);
     const hexnsec = fix0(nsec.toString(16), 8);
@@ -37,23 +35,21 @@ class TAI64N {
     for (let i = 0; i < 4; i++) {
       res[i + 8] = binnsec[i];
     }
-    res[0] = 0x40;
     return res;
   }
   static decode(tai64n) { // Uint8Array, ret [sec, nsec]
     TAI64N.check(tai64n);
-    const n1 = new Uint8Array(6);
+    const n1 = new Uint8Array(8);
     for (let i = 0; i < n1.length; i++) {
-      n1[i] = tai64n[i + 2];
+      n1[i] = tai64n[i];
     }
     const hexsec = hex.fromBin(n1);
-    n1[0] = 0;
-    n1[1] = 0;
+    const sec = BigInt("0x" + hexsec) - 0x4000000000000000n;
+    const n2 = new Uint8Array(4);
     for (let i = 0; i < 4; i++) {
-      n1[i + 2] = tai64n[i + 2 + 6];
+      n2[i] = tai64n[i + 8];
     }
-    const hexnsec = hex.fromBin(n1);
-    const sec = parseInt(hexsec, 16);
+    const hexnsec = hex.fromBin(n2);
     const nsec = parseInt(hexnsec, 16);
     const offset = TAI64N.getOffsetByLeap(sec);
     return [sec - offset, nsec];
@@ -62,10 +58,7 @@ class TAI64N {
     if (!s || s.length != 25 || s[0] != "@") {
       return null;
     }
-    if (!s.startsWith("@400000")) {
-      throw new Error("not supported yet before 1970");
-    }
-    const sec = parseInt(s.substring(1 + 6, 1 + 6 + 10), 16);
+    const sec = BigInt("0x" + s.substring(1, 1 + 16)) - 0x4000000000000000n;
     const nsec = parseInt(s.substring(1 + 16, 1 + 16 + 8), 16);
     const offset = TAI64N.getOffsetByLeap(sec);
     return TAI64N.encode(sec - offset, nsec);
@@ -77,13 +70,15 @@ class TAI64N {
   static toDate(tai64n) {
     TAI64N.check(tai64n);
     const [sec, nsec] = TAI64N.decode(tai64n);
-    return new Date(sec * 1000 + nsec / 1000000);
+    const t = sec * 1000n + BigInt(nsec) / 1000000n;
+    // check valid
+    return new Date(parseInt(t));
   }
   static getOffsetByLeap(sec) {
-    let offset = 10 + leapSeconds.length;
+    let offset = 10n + BigInt(leapSeconds.length);
     for (const l of leapSeconds) {
       offset--;
-      if (sec > l) {
+      if (sec > BigInt(l)) {
         break;
       }
     }
@@ -114,6 +109,15 @@ class TAI64N {
       }
     }
     return true;
+  }
+  static fromYear(year) {
+    year = BigInt(year);
+    const sec = (year - 1970n) * (365n * 24n * 60n * 60n);
+    return TAI64N.encode(sec, 0);
+  }
+  static toYear(tai64n) {
+    const [sec, _] = TAI64N.decode(tai64n);
+    return sec / (365n * 24n * 60n * 60n) + 1970n;
   }
 }
 
--- TAI64N.test.js
+++ TAI64N.test.js
@@ -5,14 +5,17 @@ Deno.test("parse", () => {
   const chk = (stai64n, sdate) => {
     t.assertEquals(TAI64N.toDate(TAI64N.parse(stai64n)), new Date(sdate));
   };
-  chk("@4000000037c219bf2ef02e94", "1999-08-24 04:03:43.7874925 +0000 UTC"); // https://cr.yp.to/daemontools/tai64nlocal.html
   chk("@400000000000000000000000", "1969-12-31T23:59:50.000Z"); // 1970-01-01時点で、UTCはTAIから10秒遅れていた
   chk("@400000000000000a00000000", "1970-01-01T00:00:00.000Z");
+  chk("@4000000037c219bf2ef02e94", "1999-08-24 04:03:43.7874925 +0000 UTC"); // https://cr.yp.to/daemontools/tai64nlocal.html
   chk("@4000000037c219bf2ef02e94", "1999-08-23 21:03:43.787492500 -0700"); // https://manpages.debian.org/testing/daemontools/tai64nlocal.8.en.html
 });
+
 Deno.test("offset", () => {
-  t.assertEquals(TAI64N.getOffsetByLeap(new Date().getTime()), 37); // 2019年2月現在、UTCはTAIから37秒遅れている https://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E5%8E%9F%E5%AD%90%E6%99%82
-  t.assertEquals(TAI64N.getOffsetByLeap(new Date("2038-01-01T00:00").getTime()), 37); // 2019年2月現在、UTCはTAIから37秒遅れている https://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E5%8E%9F%E5%AD%90%E6%99%82
+  t.assertEquals(TAI64N.getOffsetByLeap(new Date().getTime() / 1000), 37n); // 2019年2月現在、UTCはTAIから37秒遅れている https://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E5%8E%9F%E5%AD%90%E6%99%82
+  t.assertEquals(TAI64N.getOffsetByLeap(new Date("2038-01-01T00:00").getTime() / 1000), 37n); // 2019年2月現在、UTCはTAIから37秒遅れている https://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E5%8E%9F%E5%AD%90%E6%99%82
+  t.assertEquals(TAI64N.getOffsetByLeap(new Date("1970-01-01T00:00").getTime() / 1000), 10n); // 2019年2月現在、UTCはTAIから37秒遅れている https://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E5%8E%9F%E5%AD%90%E6%99%82
+  t.assertEquals(TAI64N.getOffsetByLeap(new Date("2000-01-01T00:00").getTime() / 1000), 32n); // 2019年2月現在、UTCはTAIから37秒遅れている https://ja.wikipedia.org/wiki/%E5%9B%BD%E9%9A%9B%E5%8E%9F%E5%AD%90%E6%99%82
 });
 Deno.test("now", () => {
   t.assertEquals(TAI64N.now().length, 12);
@@ -26,11 +29,10 @@ Deno.test("encode, stringify", () => {
 Deno.test("decode, eq", () => {
   const b = TAI64N.encode(new Date("1970-01-01T00:00:00.000Z"));
   const [sec, nsec] = TAI64N.decode(b);
-  t.assertEquals(sec, 0);
-  t.assertEquals(nsec, 0);
+  t.assertEquals([sec, nsec], [0n, 0]);
   const b2 = TAI64N.encode(new Date("2021-12-18T00:00:01.001Z"));
   const [sec2, nsec2] = TAI64N.decode(b2);
-  t.assertEquals(sec2, 1639785601);
+  t.assertEquals(sec2, 1639785601n);
   t.assertEquals(nsec2, 1000000);
   const b3 = TAI64N.encode(sec2, nsec2);
   t.assert(TAI64N.eq(b2, b3));
@@ -62,4 +64,18 @@ Deno.test("lt gt eq 2", () => {
 
   t.assert(TAI64N.eq(b1, b1));
   t.assert(TAI64N.eq(b2, b2));
+});
+Deno.test("long term", () => { // * 1 year = 365 day
+  const chk = (year, stai64n) => {
+    const tai64n = TAI64N.fromYear(year);
+    t.assertEquals(TAI64N.stringify(tai64n), stai64n);
+    t.assertEquals(TAI64N.toYear(tai64n), year);
+  };
+  chk(1n, "@3ffffff18ae2e48a00000000"); // A.D. 1
+  chk(1192n, "@3ffffffa49997d0a00000000"); // A.D. 1192
+  chk(-138n * 10000n * 10000n, "@39f5dec2799eb10a00000000"); // 138億年前 宇宙誕生
+  chk(-46n * 10000n * 10000n, "@3dfc9f8c83e0b10a00000000"); // 46億年前 地球誕生
+  chk(100n * 10000n * 10000n, "@44606231515fb12500000000"); // 100億年後
+  chk(4300n * 10000n * 10000n, "@fc3080a830cbb12500000000"); // 4300億年後 ほぼ最大値
+  chk(-1400n * 10000n * 10000n, "@02baa07493ddb10a00000000"); // -1400億年後 ほぼ最小値
 });
